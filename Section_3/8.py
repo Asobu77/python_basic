c33c4489fff42c6ed38fcd415e0b836d76cf0696
Asobu77 <43177202+Asobu77@users.noreply.github.com>
2019-06-11 20:49:59 +0900

    初回コミット　Python Udemy記録

---
+++ Section_3/8.py
@@ -0,0 +1,15 @@
+num = 1
+name = 'Milke'
+
+print(num, type(num))
+print(name, type(name))
+
+
+num = '1'
+print(num, type(num))
+num = int(num)
+print(num, type(num))
+
+# print() //表示する関数
+# type() //型を調べる関数
+# int() //タイプ変換の関数
